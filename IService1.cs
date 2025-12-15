658b9c5e9ac567f3e17791cd72313eb63efb928d
NISSI <146092409+NissiAttakora2003@users.noreply.github.com>
2025-12-15 00:06:47 +0000

    Add files via upload

---
+++ IService1.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+
+namespace WcfServiceUnitConverteer
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract] double CelsiusToFahrenheit(double celsius);
+        [OperationContract] double FahrenheitToCelsius(double fahrenheit);
+        [OperationContract] double MetersToFeet(double meters);
+        [OperationContract] double FeetToMeters(double feet);
+        [OperationContract] double KilogramsToPounds(double kilograms);
+        [OperationContract] double PoundsToKilograms(double pounds);
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,12 @@
+﻿namespace WcfServiceUnitConverteer
+{
+    public class Service1 : IService1
+    {
+        public double CelsiusToFahrenheit(double c) => (c * 9.0 / 5.0) + 32.0;
+        public double FahrenheitToCelsius(double f) => (f - 32.0) * 5.0 / 9.0;
+        public double MetersToFeet(double m) => m * 3.280839895;
+        public double FeetToMeters(double ft) => ft / 3.280839895;
+        public double KilogramsToPounds(double kg) => kg * 2.2046226218;
+        public double PoundsToKilograms(double lb) => lb / 2.2046226218;
+    }
+}
